b3d96f554836733a3f2137979471cd395a67e763
Taras <bania20091@gmail.com>
2017-04-06 10:47:05 +0300

    Order Create

--- backend/controllers/trips.js
+++ backend/controllers/trips.js
@@ -79,7 +79,7 @@ module.exports.controller = function (app) {
 
     app.post('/search', function (req, res) {
         if (!req.body) return res.sendStatus(400);
-         //console.log(req.body);
+         console.log(req.body);
         var errors = [{}];
         var result = [];
         var data = req.body;
